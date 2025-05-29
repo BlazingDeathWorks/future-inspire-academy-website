b2fec122a6a0837c545650bdbed592d008281e2a
Future Inspire Games <shunsei.chan@gmail.com>
2025-05-29 15:00:18 -0700

    Almost done with the home page

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -38,27 +38,110 @@ const Home: React.FC = () => {
               </h2>
               <div className="mt-8 space-y-8 grid grid-cols-2 md:space-y-0 gap-12 w-full max-w-full">
                 <p className="text-gray-500 sm:text-xl dark:text-gray-400 text-center">
-                  Future Inspire Academy (FIA) is a non-profit organization, transformed from a high school club, that
-                  strives to teach students how to apply their coding skills to
-                  game development in a fun and efficient way. Our mission is to
-                  create a platform to reward members who start their game
-                  development journey early.
+                  Future Inspire Academy (FIA) is a non-profit organization,
+                  transformed from a high school club, that strives to teach
+                  students how to apply their coding skills to game development
+                  in a fun and efficient way. Our mission is to create a
+                  platform to reward members who start their game development
+                  journey early.
                 </p>
                 <p className="text-gray-500 sm:text-xl dark:text-gray-400 text-center">
-                  We turned to hosting game development competitions, also known as game jams. We realized that our organization can not only impact our global members but also our national and international partners as we help promote their business, improve their products, and promote accessibility.
+                  We turned to hosting game development competitions, also known
+                  as game jams. We realized that our organization can not only
+                  impact our global members but also our national and
+                  international partners as we help promote their business,
+                  improve their products, and promote accessibility.
                 </p>
               </div>
               <p className="text-gray-500 sm:text-xl dark:text-gray-400 text-center mt-8 px-48">
-                From where we began to where we are now, we have expanded our goals to create a program that will give opportunities to aspiring game developers to enter the game development industry easier through grants, training, promotion, and exposure. We are aiming to reach greater heights to become an influential association—building careers for our members, hosting global events alongside massive corporate companies, and researching the potential of games beyond entertainment.
+                From where we began to where we are now, we have expanded our
+                goals to create a program that will give opportunities to
+                aspiring game developers to enter the game development industry
+                easier through grants, training, promotion, and exposure. We are
+                aiming to reach greater heights to become an influential
+                association—building careers for our members, hosting global
+                events alongside massive corporate companies, and researching
+                the potential of games beyond entertainment.
               </p>
             </div>
           </div>
         </section>
       </div>
       <div className="w-full border-t-2 border-t-amber-500">
-        Poop
+        <section className="">
+          <div className="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
+            <div className="max-w-full mb-8 lg:mb-16">
+              <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
+                Our Services
+              </h2>
+              <div className="mt-8 space-y-8 grid grid-cols-3 md:space-y-0 gap-12 w-full max-w-full">
+                <div className="text-center">
+                  <h2 className="text-3xl mb-4">
+                    <mark className="bg-prim text-sec font-extrabold">
+                      Daily Unity Challenges
+                    </mark>
+                  </h2>
+                  <p className="text-gray-500 sm:text-xl dark:text-gray-400 text-center">
+                    Participate in daily polls, learn something new about game
+                    dev, and build a consistent love for programming!
+                  </p>
+                </div>
+                <div className="text-center">
+                  <h2 className="text-3xl mb-4">
+                    <mark className="bg-prim text-sec font-extrabold">
+                      Weekly Game Creations
+                    </mark>
+                  </h2>
+                  <p className="text-gray-500 sm:text-xl dark:text-gray-400 text-center">
+                    Assignment, art, music, resource links, learning videos,
+                    step-by-step guides, source code, development video. Build
+                    experience as fast as possible!
+                  </p>
+                </div>
+                <div className="text-center">
+                  <h2 className="text-3xl mb-4">
+                    <mark className="bg-prim text-sec font-extrabold">
+                      Game Dev Mentoring
+                    </mark>
+                  </h2>
+                  <p className="text-gray-500 sm:text-xl dark:text-gray-400 text-center">
+                    Get help on your specific projects either from our community
+                    or personally from the founders!
+                  </p>
+                </div>
+              </div>
+              <div className="flex justify-center space-y-8 gap-12 mt-8">
+                <div className="text-center">
+                  <h2 className="text-3xl mb-4">
+                    <mark className="bg-prim text-sec font-extrabold">
+                      Professional Networking
+                    </mark>
+                  </h2>
+                  <p className="text-gray-500 sm:text-xl dark:text-gray-400 text-center">
+                    Connect with our national and international partners who
+                    have years of experience in the game development industry
+                    and can help you with anything you need!
+                  </p>
+                </div>
+                <div className="text-center">
+                  <h2 className="text-3xl mb-4">
+                    <mark className="bg-prim text-sec font-extrabold">
+                      Company Collaborations
+                    </mark>
+                  </h2>
+                  <p className="text-gray-500 sm:text-xl dark:text-gray-400 text-center">
+                    Collaborate and partner with our organization to build your
+                    vision, promote your business, and develop your platform.
+                    Level up your company’s potential by launching unique
+                    activities with us!
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
       </div>
-      <Footer superColor="#041C32"/>
+      <Footer superColor="#041C32" />
     </>
   );
 };
